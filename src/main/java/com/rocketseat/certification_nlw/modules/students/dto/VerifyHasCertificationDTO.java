06dca1287be63839fc0e5f46916cfbbf93ed87fa
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-02-12 14:39:59 -0300

    feat: criando controller de estudante

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/dto/VerifyHasCertificationDTO.java
@@ -0,0 +1,10 @@
+package com.rocketseat.certification_nlw.modules.students.dto;
+
+import lombok.Data;
+
+@Data
+
+public class VerifyHasCertificationDTO {
+  private String email;
+  private String technology;
+}
