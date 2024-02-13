0f52f1863a06c8c66a27eea57063a2f0be26326a
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-02-13 10:27:54 -0300

    feat: criando relacionamento das entidades

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -2,6 +2,14 @@ package com.rocketseat.certification_nlw.modules.students.entities;
 
 import java.util.List;
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +17,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity(name = "students")
 public class StudentEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(unique = true, nullable = false)
   private String email;
+
+  @OneToMany(mappedBy = "studentEntity")
   private List<CertificationStudentEntity> certificationStudentEntity;
 }
